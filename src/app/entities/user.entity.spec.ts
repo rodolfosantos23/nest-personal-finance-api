50e372fb144817293af0cd71de85d228e6d82891
Rodolfo Santos <rodolfosantos23@gmail.com>
2023-04-19 12:35:44 -0300

    Adding more tests for user entity

--- src/app/entities/user.entity.spec.ts
+++ src/app/entities/user.entity.spec.ts
@@ -1,12 +1,23 @@
+import { randomUUID } from 'crypto';
+import { UserInMemoryDatabaseRepository } from '../../infra/database/in-memory-database/repositories/user-inmemory.repository';
 import { UserEntity } from './user.entity';
 
+function getUserData() {
+  const randomId = randomUUID();
+
+  return {
+    id: randomId,
+    name: 'Rodolfo Santos',
+    email: 'email@gmail.com.br',
+    phone: '11988686868',
+  };
+}
+
 describe('User entity', () => {
-  it('should be abble to create an user', () => {
-    const newUser = new UserEntity({
-      name: 'Rodolfo Santos',
-      email: 'email@gmail.com.br',
-      phone: '11988686868',
-    });
+  const userRepository = new UserInMemoryDatabaseRepository();
+
+  it('should be abble to instantiate a new user entity', () => {
+    const newUser = new UserEntity(getUserData());
     expect(newUser).toBeTruthy();
   });
 
@@ -18,5 +29,27 @@ describe('User entity', () => {
         phone: '11988686868',
       });
     }).toThrow();
+  });
+
+  it('should be able to create a new user', async () => {
+    const userData = getUserData();
+    await userRepository.create(new UserEntity(userData));
+
+    const users = await userRepository.getUsers();
+    expect(users[0].name).toBe('Rodolfo Santos');
+  });
+
+  it('should be able to find a user by id', async () => {
+    const userData = getUserData();
+    await userRepository.create(new UserEntity(userData));
+    const findedUser = await userRepository.findById(userData.id);
+    expect(findedUser.id).toBe(userData.id);
+  });
+
+  it('should be able to find a user by email', async () => {
+    const userData = getUserData();
+    await userRepository.create(new UserEntity(userData));
+    const findedUser = await userRepository.findByEmail(userData.email);
+    expect(findedUser.email).toBe(userData.email);
   });
 });
--- src/infra/database/in-memory-database/repositories/user-inmemory.repository.ts
+++ src/infra/database/in-memory-database/repositories/user-inmemory.repository.ts
@@ -4,14 +4,7 @@ import { UserRepository } from 'src/app/repositories/user.repository';
 
 @Injectable()
 export class UserInMemoryDatabaseRepository implements UserRepository {
-  private users: UserEntity[] = [
-    new UserEntity({
-      id: '78a2185f-16b0-435a-a87d-021c0bd6d650',
-      name: 'Rodolfo Santos',
-      email: 'rodolfosantos23@gmail.com',
-      phone: '11983737204',
-    }),
-  ];
+  private users: UserEntity[] = [];
 
   async findByEmail(email: string): Promise<UserEntity> {
     return this.users.find((user) => user.email == email);
